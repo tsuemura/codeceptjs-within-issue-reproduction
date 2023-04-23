8f761342f6fad5df83f5f1548fc10cc08d9fb35c
Takuya Suemura <takuyasuemura@gmail.com>
2023-04-23 20:26:36 +0900

    Add comment

--- within_test.js
+++ within_test.js
@@ -2,8 +2,11 @@ Feature('within');
 
 Scenario('can select an element within the element', ({ I }) => {
   I.amOnPage("http://127.0.0.1:8080");
-  within(locate('tr').withText('Paul'), () => {
-    I.click('Reveal')
-    I.see('Paul is also a good man')
-  })
+  // If you specify locate('tr').withText('Paul')
+  // as the first argument of the within,
+  // semantic locators should find an element within the element.
+  within(locate("tr").withText("Paul"), () => {
+    I.click("Reveal");
+    I.see("Paul is also a good man");
+  });
 });
