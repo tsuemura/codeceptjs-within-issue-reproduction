4b9cca5ba1fb0aff4a10e67b13b26368112dbcd3
Takuya Suemura <takuyasuemura@gmail.com>
2023-04-23 20:22:42 +0900

    Write a reproduction test

--- within_test.js
+++ within_test.js
@@ -1,5 +1,9 @@
 Feature('within');
 
-Scenario('test something',  ({ I }) => {
-
+Scenario('can select an element within the element', ({ I }) => {
+  I.amOnPage("http://127.0.0.1:8080");
+  within(locate('tr').withText('Paul'), () => {
+    I.click('Reveal')
+    I.see('Paul is also a good man')
+  })
 });
