6d1b2c2824e98fdc2a720dc404c6ba6e2d3bacf4
Takuya Suemura <takuyasuemura@gmail.com>
2023-04-23 20:31:45 +0900

    Run test to the GH

--- within_test.js
+++ within_test.js
@@ -1,7 +1,9 @@
 Feature('within');
 
 Scenario('can select an element within the element', ({ I }) => {
-  I.amOnPage("http://127.0.0.1:8080");
+  I.amOnPage(
+    "https://tsuemura.github.io/codeceptjs-within-issue-reproduction/"
+  );
   // If you specify locate('tr').withText('Paul')
   // as the first argument of the within,
   // semantic locators should find an element within the element.
